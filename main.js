953226d46a08754bafdc5b5f8f22234b6194e5c9
Yannick Huggler <yannick.huggler@bluewin.ch>
2018-03-05 19:30:07 +0100

    added shuffle and deck function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var canvas;
 var ctx;
 
-var cards[];
+var cards;
 
 function card(value, name, suit){
 	this.value = value;
@@ -12,11 +12,34 @@ function card(value, name, suit){
 
 
 window.onload = function() {
+	var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+
 	canvas = document.getElementById('gameCanvas');
 	ctx = canvas.getContext('2d');
 
+	canvas.width = dimension[0];
+	canvas.height = dimension[1];
 	cards = deck();
+	shuffle();
+	console.log(cards[2].suit + " " + cards[2].name);
+	draw();
+	
 }
+
+
+function shuffle () {
+  var i = 0
+  var j = 0
+  var temp = null
+
+  for (i = cards.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = cards[i]
+    cards[i] = cards[j]
+    cards[j] = temp
+  }
+}
+
 
 function deck(){
 	this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
@@ -30,4 +53,21 @@ function deck(){
     }
 
     return cards;
+}
+
+function draw() {
+	ctx.beginPath();
+	ctx.moveTo(0, canvas.height/2);
+	ctx.lineTo(canvas.width, canvas.height/2);
+	ctx.stroke();
+
+	ctx.rect(canvas.width/2 - 75, 100, 150, 200);
+	ctx.stroke();
+
+}
+
+function newCardUp() {
+	setInterval(function() {
+		
+	}, 1000/30);
 }
